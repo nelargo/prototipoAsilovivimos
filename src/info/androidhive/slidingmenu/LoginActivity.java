7ffc69fd19612cc4a42b9cce28cb29caa3174361
Felipe Navarro Torres <felipe.navarrot@gmail.com>
2014-05-23 18:22:26 -0400

    Login ready

--- src/info/androidhive/slidingmenu/LoginActivity.java
+++ src/info/androidhive/slidingmenu/LoginActivity.java
@@ -2,14 +2,17 @@ package info.androidhive.slidingmenu;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity{
-	
+	TextView user,pass,forget;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -18,6 +21,19 @@ public class LoginActivity extends Activity{
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		
 		setContentView(R.layout.activity_login);
+		
+		user = (TextView)findViewById(R.id.login_user);
+		pass = (TextView)findViewById(R.id.login_pass);
+		forget = (TextView)findViewById(R.id.login_forget);
+		
+		user.setHint(Html.fromHtml("<small><small><small>"+
+				getString(R.string.user_hint)+
+				"</small></small></small>"));
+		pass.setHint(Html.fromHtml("<small><small>"+
+				getString(R.string.pass_hint)+
+				"</small></small>"));
+		forget.setTypeface(Typeface.createFromAsset(getAssets(), "bauhaus_93.ttf"));
+		
 		
 		Button b = (Button)findViewById(R.id.buttonUpload1);
 		
